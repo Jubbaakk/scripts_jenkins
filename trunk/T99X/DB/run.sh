d287ca90772a495c4fc202452f79cc95a8ce88be
Jakub Filko <filkojakub@gmail.com>
2020-04-02 23:19:07 +0200

    Fix name

--- trunk/T99X/DB/run.sh
+++ trunk/T99X/DB/run.sh
@@ -1,3 +1,3 @@
 mkdir -p /srv/data/remote/ipc/.patchdir/logs/
 echo "-- 2020.03.05_15:18:56 IN: End of ETL_OWNER ... /srv/data/int/infa/ininfata/remote/ipc/.patchdir/.pipeline/T99X/INC999/DB/01_PRE/ETL_OWNER/test1.sql RC: 0
--- 2020.03.05_15:18:56 IN: End of ETL_OWNER ... /srv/data/int/infa/ininfata/remote/ipc/.patchdir/.pipeline/T99X/INC999/DB/01_PRE/ETL_OWNER/test2.sql RC: 0" > /srv/data/remote/ipc/.patchdir/logs/RunSqlScripts_RTOTA_T99X_INC040_2020.04.03.*.log.summary
+-- 2020.03.05_15:18:56 IN: End of ETL_OWNER ... /srv/data/int/infa/ininfata/remote/ipc/.patchdir/.pipeline/T99X/INC999/DB/01_PRE/ETL_OWNER/test2.sql RC: 0" > /srv/data/remote/ipc/.patchdir/logs/RunSqlScripts_RTOTA_T99X_INC040_2020.04.02.*.log.summary
